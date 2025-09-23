9398c595d48f52832d40b3513df5fb0e36776637
Matthew Wong <matthewwong@Matthews-MacBook-Air-3.local>
2025-09-22 20:38:44 -0700

    add script to modify subject 2 16x16

---
+++ Dbar_texp_Trig_ACT5_human_circle/Subject_2_Dbar_texp_Trig_ACT5_human_circle_Lhat.m
@@ -0,0 +1,19 @@
+%===================================================================================================
+%======================================== Specify External Data ====================================
+%===================================================================================================
+clear all
+% Directory where data is stored:
+datadir = 'ACT5_humanData/';
+
+% File name for .mat file containing EIT data 
+datafname = 'Sbj02_2D_16e_24_10_16_12_38_03_93750';  
+
+load([datadir, datafname]);
+cur_pattern = cur_pattern(1:16,1:16);
+frame_voltage = frame_voltage(1:16,1:16,:);
+
+save([datadir,'modified_16x16_',datafname])
+
+
+
+
